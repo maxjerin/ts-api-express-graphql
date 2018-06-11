af32f6a77e03a3d52877a528e1f60ace0ef95ab0
Jerin Mathew <maxjerin@gmail.com>
2018-06-11 12:04:17 -0400

    setup client using parcel

---
+++ client/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+
+const Index = () => {
+  return 'Hello React!'
+};
+
+ReactDOM.render(<Index />, document.getElementById('index'))

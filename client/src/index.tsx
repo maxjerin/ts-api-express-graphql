242fc09f1a2aa2d7a8dbbeffe599f787281574c7
Jerin Mathew <maxjerin@gmail.com>
2018-06-17 23:27:09 -0400

    changes to make webpack work

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,12 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { hot } from 'react-hot-loader'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader';
 
 const Index = hot(module)((): JSX.Element => {
-  return <div>{'Hello React!!'}</div>
+  return <div>{'Hello React!@@'}</div>
 });
 
-ReactDOM.render(<Index />, document.getElementById('index'))
+ReactDOM.render(
+  <Index />,
+  document.getElementById('index') as HTMLElement
+);

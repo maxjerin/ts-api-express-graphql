ea5c20116222162a9ffdc3f16581101a9f03a52f
Jerin Mathew <maxjerin@gmail.com>
2018-06-18 20:03:58 -0700

    create login page

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,7 +1,53 @@
 import * as React from 'react';
+import { ChangeEvent } from 'react';
+import { user } from '../../services';
 
-const Login = (): JSX.Element => {
-    return <div>{'Hello React Login!@!'} </div>;
+const initialState = {
+    username: '',
+    password: '',
 };
 
-export default Login;
+type State = Readonly<typeof initialState>;
+
+export default class Login extends React.Component<object, State> {
+    readonly state: State = initialState;
+
+    private onInputChange = (e: ChangeEvent<HTMLInputElement>, name: keyof State): void => {
+        this.setState({
+            [name]: e.target.value
+        } as any); // https://github.com/Microsoft/TypeScript/issues/5579
+    };
+
+    private login = (): void => {
+        const { username, password } = this.state;
+        user.login(username, password);
+    };
+
+    render(): JSX.Element {
+        const { username, password } = this.state;
+
+        return (
+            <div>
+                <div>
+                    <input
+                        type="textbox"
+                        placeholder="Username"
+                        id="username"
+                        onChange={(e) => this.onInputChange(e, 'username')}
+                        value={username}
+                    />
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={(e) => this.onInputChange(e, 'password')}
+                        placeholder="Password"
+                        value={password}
+                    />
+                </div>
+                <div>
+                    <button onClick={this.login}>Submit</button>
+                </div>
+            </div>
+        );
+    }
+}

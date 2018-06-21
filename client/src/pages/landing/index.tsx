b6a2990fd1c0fe3b625507a623753c3a6402d5e1
Jerin Mathew <maxjerin@gmail.com>
2018-06-20 23:57:16 -0400

    setup react-router

--- client/src/pages/landing/index.tsx
+++ client/src/pages/landing/index.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import Login from '../login';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+const history = createBrowserHistory();
 
 const Landing = (): JSX.Element => {
     return (
         <div onClick={() => console.warn('why you no accept')}>
-            <Login />
-            {'Hello React Hot Loader!!'}
-        </div>
+            <BrowserRouter>
+                <Route path="/" component={Login} />
+                {/* <Route path="/memberEdit/:id" component={MemberPageContainer} /> */}
+            </BrowserRouter>
+        </div >
     );
 };
 
